6897c17d6d170cd0d15ab2a35ecd3093b1122d22
AnilMertTuremis <101046999+AnilMertTuremis@users.noreply.github.com>
2022-05-27 13:20:15 +0300

    Add files via upload

---
+++ bounswe-master/app/quora/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import (
+    Answer,
+    AnswerAdmin,
+    Author,
+    AuthorAdmin,
+    Comment,
+    CommentAdmin,
+    Question,
+    QuestionAdmin,
+)
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Comment, CommentAdmin)
